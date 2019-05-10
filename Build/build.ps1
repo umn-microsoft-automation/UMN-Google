6d6ba42a5eef0eb701f2f1548a8b37ee498c9a60
Peter Bajurny <bajurny@umn.edu>
2019-05-10 15:23:18 -0500

    Update build.ps1

--- Build/build.ps1
+++ Build/build.ps1
@@ -2,19 +2,12 @@ param ($Task = 'Default')
 $VerbosePreference = 'Continue'
 
 # Grab nuget bits, install modules, set build variables, start build.
-Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+#Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
 
 Install-Module Psake, PSDeploy, BuildHelpers, Pester -Force
 Import-Module Psake, BuildHelpers
 
-#get-command 'git'
-#write-warning $PWD.Path
-
-Get-Item Env:
-(get-item Env:BUILD_SourceVersionMessage).Value
-
-
-Set-BuildEnvironment
+#Set-BuildEnvironment
 
 Invoke-psake -buildFile .\Build\psake.ps1 -taskList $Task -nologo
 exit ( [int]( -not $psake.build_success ) )
