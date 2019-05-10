79e47f51f4652f584adf02c988d4511737cd5200
Peter Bajurny <bajurny@umn.edu>
2019-05-10 09:36:12 -0500

    added build scriptsw

---
+++ Build/build.ps1
@@ -0,0 +1,16 @@
+param ($Task = 'Default')
+$VerbosePreference = 'Continue'
+
+# Grab nuget bits, install modules, set build variables, start build.
+Get-PackageProvider -Name NuGet -ForceBootstrap | Out-Null
+
+Install-Module Psake, PSDeploy, BuildHelpers, Pester -Force
+Import-Module Psake, BuildHelpers
+
+#get-command 'git'
+#write-warning $PWD.Path
+
+Set-BuildEnvironment
+
+Invoke-psake -buildFile .\Build\psake.ps1 -taskList $Task -nologo
+exit ( [int]( -not $psake.build_success ) )
